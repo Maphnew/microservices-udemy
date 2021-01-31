cc9f602899fed360be6367faf1f5364444370aab
maphnew <zcm3118@gmail.com>
2021-01-31 12:38:41 +0900

    48. Event Syncing in Action

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -6,15 +6,24 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+let events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    events.push(event);
 
+    axios.post('http://localhost:4000/events', event).catch(e => console.log(e.message));
+    axios.post('http://localhost:4001/events', event).catch(e => console.log(e.message));
+    axios.post('http://localhost:4002/events', event).catch(e => console.log(e.message));
+    axios.post('http://localhost:4003/events', event).catch(e => console.log(e.message));
+    
     res.send({ status: 'OK' });
 });
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.listen(4005, () => {
     console.log('Listening on 4005')
