96742de0d853757bf3a0f2e4cbc864a80d332abc
maphnew <zcm3118@gmail.com>
2021-02-07 12:49:49 +0900

    85. Writing Ingress Config Files

--- blog/query/index.js
+++ blog/query/index.js
@@ -52,7 +52,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
     console.log('Listening on 4002');
 
-    const res = await axios.get('http://localhost:4005/events').catch(e => console.log(e.message));
+    const res = await axios.get('http://event-bus-srv:4005/events').catch(e => console.log(e.message));
 
     for(let event of res.data) {
         console.log('Processing event:', event.type)
